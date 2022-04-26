4462bfa1a2701114cd78bf332ae934f55daf4a7f
Thiago Pedais <luizthiago20sccp@gmail.com>
2022-04-26 15:07:33 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/clients/projectClients/ProjectClientsApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.clients.projectClients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectClientsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/clients/projectClients/ProjectClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.clients.projectClients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
